67cb833d3f7702c96ea760db061d0410c8a5e872
Nordwall <tobiasnordwall@gmail.com>
2022-01-20 10:41:30 +0100

    i have added so that the active page is highlighted by using a ternary operator that checks if what page you are on and the data is sent with the render function

--- app.js
+++ app.js
@@ -24,39 +24,39 @@ app.use(express.static(__dirname + "/static"));
 app.listen(port);
 
 app.get("/", async (req, res) => {
-    res.render("index"); 
+    res.render("index", { page_name: "start"}); 
 }); 
 
 app.get("/movies", async (req, res) => {
     const movies = await loadMovies();
-    res.render("moviepage", { movies, layout: "./layouts/movie-layout"}); 
+    res.render("moviepage", { movies, layout: "./layouts/movie-layout", page_name: "movies"}); 
 })
 
 app.get("/movies/:movieId", async (req, res) => {
     const movie = await loadMovie(req.params.movieId);
     if (movie) {
-      res.render("movie", { movie, layout: "./layouts/movie-layout"});
+      res.render("movie", { movie, layout: "./layouts/movie-layout", page_name: "movies"});
     } else {
       res.status(404).render("404page");
     }
   });
 
 app.get(["/aboutus", "/aboutus/history"], (req, res) => {
-    res.render("aboutus-history", {layout: "./layouts/aboutus-layout"} )
+    res.render("aboutus-history", {layout: "./layouts/aboutus-layout", page_name: "aboutus", underpage_name: "history"} )
 });
 
 app.get("/aboutus/faq", (req, res) => {
-  res.render("aboutus-faq", {layout: "./layouts/aboutus-layout"} )
+  res.render("aboutus-faq", {layout: "./layouts/aboutus-layout", page_name: "aboutus", underpage_name: "faq"} )
 });
 
 app.get("/aboutus/accessibility", (req, res) => {
-  res.render("aboutus-accessibility", {layout: "./layouts/aboutus-layout"} )
+  res.render("aboutus-accessibility", {layout: "./layouts/aboutus-layout", page_name: "aboutus", underpage_name: "accessibility"} )
 });
 
 app.get("/aboutus/policy", (req, res) => {
-  res.render("aboutus-policy", {layout: "./layouts/aboutus-layout"} )
+  res.render("aboutus-policy", {layout: "./layouts/aboutus-layout", page_name: "aboutus", underpage_name: "policy"} )
 });
 
 app.use((req, res) => {
-    res.status(404).render("404page");
+    res.status(404).render("404page", { page_name: "error" });
 });
